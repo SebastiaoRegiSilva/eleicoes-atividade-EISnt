fe3ff14d65809f7c4c5ad5455dcb80358e85f72c
Sebastião Willian <swillian2009@gmail.com>
2024-11-26 22:58:48 +0000

    Buscando desempatar para finalizar a aplicação.

--- Program.cs
+++ Program.cs
@@ -6,72 +6,134 @@ namespace Eleicoes.Atividade.EISnt
 {
     class Program
     {
-        static List<int> numerocandidato = new List<int> { 1, 2, 3, 4 };
+        static List<int> numeroCandidatos = new List<int> { 
+            1, 
+            2, 
+            3, 
+            4 
+        };
         /// <summary>
         /// Contador de votos.
         /// </summary>
         /// <typeparam name="int">Número do candidato.</typeparam>
         /// <typeparam name="int">Votos computados ao candidato.</typeparam>
         /// <returns></returns>
-        static Dictionary<int, int> votosValidos = new Dictionary<int, int>();
+        static Dictionary<int, int> votosValidos = new Dictionary<int, int>{
+            { 1, 0 },
+            { 2, 0 },
+            { 3, 0 },
+            { 4, 0 }
+        };
 
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Eleições 2024 - Sejam bem-vindos!");
-            Console.ResetColor();
-            Console.WriteLine("Selecione seu voto por gentileza.");
-            int numeroCandidato = ValidarEntrada(Console.ReadLine());
-            
-            AdicionarVotos(votosValidos, numeroCandidato);
-
-            // Exibe os resultados da eleição.
-            foreach (var candidato in votosValidos)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Candidato: {candidato.Key}, Valor: {candidato.Value}");
-                Console.ResetColor();
-            }
-
-            Console.ReadKey();
+            TelaInicial();
         }
-
-        private static int ValidarEntrada(string? entradaUsuario)
+        
+        private static int ValidarEntrada()
         {
-            int numero;
+            int numeroValido = 0;
             bool entradaValida = false;
 
             while (!entradaValida)
             {
-                Console.Write("Digite um número entre 1 e 4: ");
-                entradaUsuario = Console.ReadLine();
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Digite um número entre 1 e 4:");
+                Console.ResetColor();
+                string entrada = Console.ReadLine()?.Trim(); // Remove espaços em branco do início e do fim.
 
-                // Tenta converter a entrada para inteiro
-                if (int.TryParse(entradaUsuario, out numero))
-                {
-                    // Verifica se o número está entre 1 e 4
-                    if (numero >= 1 && numero <= 4)
-                    {
-                        entradaValida = true;
-                        Console.WriteLine($"Você digitou um número válido: {numero}");
-                    }
-                    else
-                        Console.WriteLine("O número deve estar entre 1 e 4. Tente novamente.");
+                if (int.TryParse(entrada, out numeroValido) && numeroValido >= 1 && numeroValido <= 4)
+                    entradaValida = true;
+                else if(numeroValido == 0){
+                    ImprimirResultado();
+                    Console.WriteLine("Digite CRTL + C, para encerar a aplicação!");
+                    string vazia = Console.ReadLine(); // Pausa da aplicação.
                 }
-                else
-                    Console.WriteLine("Entrada inválida. Por favor, digite um número.");
-                
+                else{
+                    try{
+                        Console.Clear();
+                    }
+                    catch (Exception){
+                        Console.WriteLine("Candidato inexistente, digite novamente.");
+                    }
+                    finally{
+                        ImprimirInicio();
+                        ImprimirUrnaVotacao();
+                    }
+                }
+            }
+            return numeroValido;
+        }
+
+        static void AdicionarVotos(int numeroCandidato, int valor = 1)
+        {
+            if (numeroCandidato != 0 && numeroCandidatos.Contains(numeroCandidato)){
+                foreach (var numero in numeroCandidatos.Where(x => x.Equals(numeroCandidato)))
+                    votosValidos[numeroCandidato] += valor;
             }
         }
-        
-        static void AdicionarVotos(Dictionary<string, int> votosComputar, int numeroCandidato, int valor = 1)
-        {
-            if (numeroCandidato != 0 && votosComputar.ElementAt(n => n == numeroCandidato))
-            {
-                if (votosValidos.ContainsKey(numeroCandidato))
-                    // Incrementa o valor a chave existente.
-                    votosComputar[numeroCandidato] += valor;
+
+        /*
+            BLOCO DE IMPRESSÕES.
+        */
+
+        private static void TelaInicial(){
+            ImprimirInicio();
+            while(ImprimirUrnaVotacao() != 0){
+                ImprimirUrnaVotacao();
             }
+        }
+
+        private static void ImprimirInicio(){
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Eleições 2024 - Sejam bem-vindos!");
+            Console.ResetColor();
+        }
+        
+        private static int ImprimirUrnaVotacao(){
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Escolha seu candidato, por gentileza.");
+            int numeroCandidato = ValidarEntrada();
+            AdicionarVotos(numeroCandidato);
+            Console.ResetColor();
+            return numeroCandidato;
+        }
+
+        private static void ImprimirResultado(){
+            
+            var vencedor = votosValidos.Aggregate((x, y) => x.Value > y.Value ? x : y);
+            // Desempatar...
+            // Exibe os resultados da eleição.
+            foreach (var candidato in votosValidos)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Candidato: {candidato.Key}, Valor: {candidato.Value}");
+            }
+
+            Console.WriteLine($"O Candidato com número: {vencedor.Key}, venceu com: {vencedor.Value}votos! ");
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Função adicional para tirar igualdade entre candidatos.
+        /// </summary> <summary>
+        private static int Desempatar()
+        {
+            int desempate =0, maximoValorVotos = votosValidos.Values.Max();
+            
+            // Agrupando por valores e selecionando apenas os valores repetidos
+            var valores = votosValidos
+                .GroupBy(pair => pair.Value)  // Agrupa pelos valores
+                .Where(group => group.Count() > 1); // Seleciona grupos com mais de uma ocorrência
+
+            // Exibindo as chaves e valores duplicados
+            foreach (var group in valores)
+            {
+                var maxPair = group.First(pair => pair.Value == maximoValorVotos);
+                desempate = maxPair.Value + 1;
+            }
+
+            return desempate;
         }
     }
 }
